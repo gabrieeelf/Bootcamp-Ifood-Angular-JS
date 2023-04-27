ba70cfd4e24313da0ca83e4e869ce352b320b56a
Gabriel Gomes <bielfgq@gmail.com>
2023-04-26 21:29:14 -0300

    Aula sobre Promisse - 26/04/2023

--- Conteúdos/Prog. Web/Pokedex/assets/js/main.js
+++ Conteúdos/Prog. Web/Pokedex/assets/js/main.js
@@ -1,1 +1,8 @@
-console.log("Sucesso!");
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+fetch(url)
+    .then((response) => response.json())                                        //Executa os comandos 
+    .then((jsonBody) => console.log(jsonBody))                                  //Executa os comandos
+    .catch((error) => console.error(error))                                     //Acusa caso algo der erro
