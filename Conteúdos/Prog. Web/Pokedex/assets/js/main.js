f3aa2b0da9431def841940f1391beaabfa570a4b
Gabriel Gomes <bielfgq@gmail.com>
2023-04-27 22:01:46 -0300

    Refatorando JS

--- Conteúdos/Prog. Web/Pokedex/assets/js/main.js
+++ Conteúdos/Prog. Web/Pokedex/assets/js/main.js
@@ -1,7 +1,3 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-
 function convertPokemonToLi(pokemon) {
     return `<li class="pokemon">
                 <span class="number">#001</span>
@@ -21,18 +17,13 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
-    .then((response) => response.json()) //Executa os comandos 
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
+pokeApi.getPokemons().then((pokemons) => {
+    const listItems = []
 
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        listItems.push(convertPokemonToLi(pokemon))
+    }
 
-            pokemonList.innerHTML += convertPokemonToLi(pokemon)
-
-        }
-
-    })
-
-.catch((error) => console.error(error)) //Acusa caso algo der erro
+    console.log(listItems)
+})
