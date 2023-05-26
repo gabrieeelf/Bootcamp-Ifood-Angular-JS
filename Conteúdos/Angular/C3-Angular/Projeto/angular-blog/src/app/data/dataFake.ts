bd469924bff75dfe511ffa847fe50849522e6fbb
Gabriel Gomes <bielfgq@gmail.com>
2023-05-25 21:22:20 -0300

    Blog Criado

--- Conteúdos/Angular/C3-Angular/Projeto/angular-blog/src/app/data/dataFake.ts
+++ Conteúdos/Angular/C3-Angular/Projeto/angular-blog/src/app/data/dataFake.ts
@@ -1,20 +1,92 @@
 export const dataFake = [
   {
-    "id": 1,
-    "photo": "aaa",
-    "title": "Vai timão",
-    "description": "AAAAAAAAAAA"
+    "id": "1",
+    "title": "Sport Club Corinthians Paulista",
+    "description": `O Corinthians, oficialmente conhecido como Sport Club Corinthians Paulista, é um dos clubes de futebol mais populares e importantes do Brasil. Fundado em 1º de setembro de 1910, na cidade de São Paulo, o clube tem uma história rica em conquistas e uma base de torcedores extremamente fiel e apaixonada.
+
+    O Corinthians é reconhecido por suas cores preto e branco e pelo escudo com a âncora, símbolo que representa a força e estabilidade do clube. Sua torcida, conhecida como "Fiel Torcida", é uma das mais numerosas e fanáticas do país, sempre presente em grande número nos estádios e apoiando o time com fervor.
+
+    O clube conquistou uma série de títulos importantes ao longo de sua história. Destacam-se as sete vezes em que foi campeão brasileiro, em 1990, 1998, 1999, 2005, 2011, 2015 e 2017. Além disso, o Corinthians venceu a Copa Libertadores da América em duas ocasiões, em 1990 e 2012, e foi campeão mundial em 2000 e 2012.
+
+    No âmbito estadual, o Corinthians é um dos maiores vencedores do Campeonato Paulista, com 30 títulos conquistados até 2021. Essas conquistas consolidaram sua posição como um dos clubes mais tradicionais e bem-sucedidos do futebol paulista e brasileiro.
+
+    Além do sucesso em campo, o Corinthians também tem uma importância social significativa. O clube mantém projetos sociais e comunitários, usando o esporte como uma ferramenta para promover o desenvolvimento de jovens atletas e realizar ações de responsabilidade social.
+
+    A rivalidade do Corinthians com outros grandes clubes paulistas, como Palmeiras, São Paulo e Santos, é uma marca importante do futebol brasileiro. Os jogos entre essas equipes, conhecidos como "clássicos", são sempre disputados e carregados de emoção, despertando paixão nos torcedores.
+
+    Em resumo, o Corinthians é uma instituição esportiva de renome, com uma história repleta de conquistas, uma torcida apaixonada e uma posição de destaque no cenário do futebol brasileiro. Seu legado e sua importância vão além do esporte, influenciando a cultura e a identidade da cidade de São Paulo e do país como um todo.`,
+    "photoCover": "https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt46a0433af52c06a5/64386792311bf70dbf772637/SCCPxNike_Home_Details_1x1_1.jpg?auto=webp&format=pjpg&width=3840&quality=60"
   },
   {
-    "id": 2,
-    "photo": "aaa",
-    "title": "Vai timão",
-    "description": "AAAAAAAAAAA"
+    "id": "2",
+    "title": "Libertadores 2012",
+    "description": `A conquista da Copa Libertadores da América em 2012 foi um marco histórico para o Corinthians e sua torcida. O clube alcançou o título mais desejado do continente sul-americano, consolidando-se como campeão do torneio mais prestigioso da região.
+
+    A campanha do Corinthians na Libertadores de 2012 foi marcada por grandes desafios e momentos emocionantes. Sob o comando do técnico Tite, o time demonstrou determinação e solidez ao enfrentar adversários difíceis e superar obstáculos ao longo da competição.
+
+    Na fase de grupos, o Corinthians mostrou sua força e avançou para as etapas eliminatórias com destaque. Nas oitavas de final, enfrentou o Emelec, do Equador, em confronto acirrado. Após dois empates, a decisão foi para os pênaltis, e o Corinthians prevaleceu, garantindo sua classificação.
+
+    Nas quartas de final, o adversário foi o Vasco da Gama, em duelos eletrizantes que também foram decididos nas penalidades máximas. Novamente, o Corinthians se sobressaiu e avançou para a semifinal.
+
+    Na semifinal, o desafio era o arquirrival Santos, em um confronto que despertou grande expectativa. Em dois jogos emocionantes, o Corinthians mostrou sua superioridade e venceu a equipe santista, garantindo sua passagem para a grande final da Libertadores.
+
+    Na final, o Corinthians enfrentou o Club Atlético Boca Juniors, da Argentina, uma equipe tradicional e com uma história vitoriosa na competição. O primeiro jogo, disputado em Buenos Aires, terminou com um empate sem gols, deixando a decisão em aberto para o confronto de volta.
+
+    No dia 4 de julho de 2012, o estádio do Pacaembu, em São Paulo, foi palco de uma noite inesquecível para a torcida corintiana. Em uma atuação brilhante, o Corinthians venceu o Boca Juniors por 2 a 0, com gols de Emerson Sheik e Chicão. O clube coroou sua trajetória na Libertadores com uma vitória convincente e a conquista do título inédito.
+
+    A vitória na Copa Libertadores da América em 2012 marcou não apenas a história do Corinthians, mas também a história do futebol brasileiro. Foi a primeira vez que o clube paulista conquistou o torneio continental, e a torcida celebrou com entusiasmo e emoção esse momento tão especial.
+
+    A conquista da Libertadores abriu as portas para o Corinthians disputar o Campeonato Mundial de Clubes da FIFA, realizado no final do ano. Em uma campanha sólida, o clube superou adversários internacionais e conquistou o título mundial, adicionando ainda mais glória a uma temporada memorável.
+
+    A Libertadores de 2012 ficou marcada na memória dos torcedores corintianos como um capítulo histórico e emocionante. O Corinthians provou seu valor, superou desafios e levou o nome do clube e da torcida para o cenário internacional, deixando um legado de conquista e orgulho.`,
+    "photoCover": "https://static.corinthians.com.br/uploads/16252647319657c1fffd38824e5ab0472e022e577e.jpg"
   },
   {
-    "id": 3,
-    "photo": "aaa",
-    "title": "Vai timão",
-    "description": "AAAAAAAAAAA"
+    "id": "3",
+    "title": "Mundial 2012",
+    "description": `O título do Campeonato Mundial de Clubes da FIFA em 2012 foi um momento histórico e inesquecível para o Corinthians e sua torcida. O clube conquistou o título de campeão mundial, provando sua grandeza e competência em nível global.
+
+    Após vencer a Copa Libertadores da América no mesmo ano, o Corinthians garantiu sua participação no Mundial de Clubes, realizado no Japão. O torneio reuniu as equipes campeãs de cada continente, proporcionando confrontos emocionantes e o prestígio de ser considerado o melhor time do mundo.
+
+    A campanha corintiana no Mundial de 2012 foi marcada por uma determinação e foco impressionantes. O time, comandado pelo técnico Tite, enfrentou grandes desafios e demonstrou sua qualidade em cada partida disputada.
+
+    Na semifinal, o Corinthians enfrentou o Al-Ahly, campeão da Liga dos Campeões da África. Em um jogo controlado e eficiente, o clube brasileiro venceu por 1 a 0, com um gol marcado por Paolo Guerrero, garantindo sua vaga na final.
+
+    Na grande final, o Corinthians enfrentou o Chelsea, campeão da Liga dos Campeões da UEFA. O confronto despertou grande expectativa, pois o clube inglês era considerado um dos melhores times da época.
+
+    No dia 16 de dezembro de 2012, no Estádio Internacional de Yokohama, as duas equipes protagonizaram uma partida intensa e equilibrada. Após um empate em 1 a 1 no tempo normal, o jogo foi para a prorrogação.
+
+    Foi nesse momento que a estrela de Paolo Guerrero brilhou novamente. Aos 14 minutos do primeiro tempo da prorrogação, o atacante peruano marcou o gol que colocou o Corinthians à frente no placar. Com uma atuação defensiva sólida e uma liderança exemplar, o time brasileiro conseguiu segurar o resultado até o apito final.
+
+    A vitória por 1 a 0 sobre o Chelsea consagrou o Corinthians como campeão mundial. A conquista levou a torcida alvinegra à loucura, comemorando um título inédito e a confirmação do status do clube como um dos melhores do mundo.
+
+    O título mundial de 2012 marcou um momento de glória para o Corinthians e seu legado no futebol. A conquista demonstrou a capacidade do clube em competir em nível internacional e eternizou a equipe e seus jogadores na história do futebol brasileiro.
+
+    O Corinthians se tornou o segundo clube brasileiro a conquistar o título mundial, reforçando sua grandeza e deixando uma marca indelével na trajetória do clube. A vitória no Mundial de 2012 é lembrada com orgulho pela torcida corintiana, como um símbolo de excelência e conquista em escala global.`,
+    "photoCover": "https://static-wp-tor15-prd.torcedores.com/wp-content/uploads/2017/12/corinthians-2.jpg"
+  },
+  {
+    "id": "4",
+    "title": "Paulista 2017",
+    "description": `O Campeonato Paulista de 2017 foi uma competição marcante na história do Corinthians. O clube conquistou o título estadual de forma invicta, realizando uma campanha impecável e consolidando sua superioridade dentro de campo.
+
+    Sob o comando do técnico Fábio Carille, o Corinthians demonstrou um estilo de jogo sólido e consistente ao longo da competição. Desde as primeiras rodadas, o time mostrou sua força, conquistando vitórias importantes e acumulando pontos de forma regular.
+
+    Na fase de grupos, o Corinthians terminou na primeira colocação do seu grupo, com uma campanha impressionante. Foram 26 pontos em 12 jogos, com 8 vitórias e 2 empates, mostrando sua superioridade sobre os adversários.
+
+    Nas quartas de final, o clube enfrentou o Botafogo-SP, em confrontos que confirmaram a supremacia corintiana. Com duas vitórias por 1 a 0, o Corinthians avançou para a semifinal.
+
+    Na semifinal, o adversário foi o São Paulo, em um clássico que despertou grande expectativa. No primeiro jogo, disputado no Morumbi, o Corinthians empatou por 1 a 1. Já no jogo de volta, realizado na Arena Corinthians, a equipe alvinegra mostrou sua força e venceu por 1 a 0, garantindo sua vaga na grande final.
+
+    A final do Paulistão 2017 foi um encontro histórico entre Corinthians e Ponte Preta. No primeiro jogo, em Campinas, o Corinthians conquistou uma vitória por 3 a 0, deixando uma vantagem confortável para o jogo de volta.
+
+    Na segunda partida, realizada na Arena Corinthians, o clube alvinegro consolidou sua superioridade ao vencer novamente por 1 a 0, com gol de Jadson. Com o resultado, o Corinthians sagrou-se campeão paulista de forma invicta, coroando uma campanha brilhante e dominante.
+
+    O título do Campeonato Paulista de 2017 marcou o 28º título estadual do Corinthians, reforçando sua tradição como um dos clubes mais vitoriosos do estado de São Paulo. Além disso, a conquista invicta trouxe ainda mais prestígio ao clube e à sua torcida apaixonada.
+
+    A campanha vitoriosa do Corinthians no Paulistão 2017 evidenciou a força do time, sua organização tática e a capacidade de superar desafios. O título foi celebrado intensamente pela torcida corintiana, que viu seu clube dominar o futebol estadual e mostrar-se imbatível em campo.
+
+    O Paulistão 2017 entrou para a história do Corinthians como um marco de sucesso e supremacia no cenário do futebol paulista, consolidando ainda mais o clube como uma potência do estado e do país.`,
+    "photoCover": "https://static.corinthians.com.br/uploads/1620080821e7a0ac723159df05cb1edaa7683e1a53.jpg"
   }
 ]
--- Conteúdos/Angular/C3-Angular/Projeto/angular-blog/src/app/pages/content/content.component.ts
+++ Conteúdos/Angular/C3-Angular/Projeto/angular-blog/src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {dataFake} from '../../data/dataFake'
 
 @Component({
   selector: 'app-content',
@@ -12,14 +13,25 @@ export class ContentComponent implements OnInit {
   contentTitle:string = "NOTICIA EXEMPLO";
   contentDescription:string = "TESTEEEEEEEEEEEEEEEE";
 
+  private id: string | null = "0";
+
   constructor(
     private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(value =>
-      console.log(value.get("Id"))
+      this.id = value.get("id")
     )
+
+    this.setValuesToComponent(this.id);
   }
 
+  setValuesToComponent(id:string | null){
+    const result = dataFake.filter(article => article.id == id)[0]
+
+    this.contentTitle = result.title;
+    this.contentDescription = result.description;
+    this.photoCover = result.photoCover;
+  }
 }
